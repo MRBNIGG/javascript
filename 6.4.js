c7ff7dc458bfa8a7f3733e5ca818156417cf0ef4
MRBNIGG <113429392+MRBNIGG@users.noreply.github.com>
2022-11-22 13:43:32 +0100

    Add files via upload

---
+++ 6.4.js
@@ -0,0 +1,17 @@
+function calculator(num1, num2) {
+    let answer = num1 + num2;
+    console.log(answer = 13);
+    alert("13");
+}
+
+function multiplyByFive() {
+    getal1 = prompt("geef een getal");
+    let answer = getal1 * 5; 
+    console.log(answer);
+}
+
+function calculateMinutes() {
+    getal1 = prompt("geef een getal");
+    let answer = getal1 / 60;
+    console.log(answer);
+}
