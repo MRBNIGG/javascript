c46c041c567a2ea3a4224794dc57f9d65aa7885f
MRBNIGG <113429392+MRBNIGG@users.noreply.github.com>
2022-11-22 13:40:50 +0100

    Add files via upload

---
+++ 6.3.js
@@ -0,0 +1,6 @@
+function kleur_aanpassen(){
+    document.getElementById("kleur").style.color = "blue";
+    document.getElementById("kleur").innerText = "Inhoud aangepast";
+}
+
+kleur_aanpassen();
