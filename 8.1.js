933156bc962071d06396601a8b3016a2e11d2525
MRBNIGG <113429392+MRBNIGG@users.noreply.github.com>
2022-11-22 13:58:42 +0100

    Add files via upload

---
+++ 8.1.js
@@ -0,0 +1,29 @@
+function add()
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a+b
+
+}
+
+function minus() 
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a-b
+
+
+}
+function multiply() 
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a*b
+
+
+}
+
+function devide() 
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a/b
+
+
+}
